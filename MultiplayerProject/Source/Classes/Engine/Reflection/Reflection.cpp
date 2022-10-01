bd8d6b506f22a0351eccf78df16ac99cef79f590
rlindsa@masonlive.gmu.edu <ryanlindsay196@gmail.com>
2022-10-01 15:58:04 -0400

    Added function reflection for instances of entities.

--- MultiplayerProject/Source/Classes/Engine/Reflection/Reflection.cpp
+++ MultiplayerProject/Source/Classes/Engine/Reflection/Reflection.cpp
@@ -1,6 +1,6 @@
 #include "Engine/Reflection/Reflection.h"
 
-const MetaType& GetGlobalMetaType(int)
+const MetaType& GetGlobalMetaType(const int&)
 {
 	static IntMetaType intMetaType;  
 	return intMetaType;
@@ -16,4 +16,36 @@ const MetaType & GetGlobalMetaType(const double &)
 {
 	static DoubleMetaType doubleMetaType;
 	return doubleMetaType;
+}
+
+const MetaType& GetGlobalMetaType(void*)
+{
+	static VoidMetaType metaType;
+	return metaType;
+}
+
+template<>
+const MetaType& GetMetaTypeByType<void>()
+{
+	void* v = nullptr;
+	return GetGlobalMetaType(v);
+}
+
+void IntMetaType::Cast(void * to, void * from, const MetaType & fromType) const
+{
+	*(int*)to = (int)fromType.ToNumber(from);
+}
+
+void FloatMetaType::Cast(void * to, void * from, const MetaType & fromType) const
+{
+	*(float*)to = (float)fromType.ToNumber(from);
+}
+
+void DoubleMetaType::Cast(void * to, void * from, const MetaType & fromType) const
+{
+	*(double*)to = (double)fromType.ToNumber(from);
+}
+
+void VoidMetaType::Cast(void * to, void * from, const MetaType & fromType) const
+{
 }
--- MultiplayerProject/Source/Headers/Engine/Reflection/Reflection.h
+++ MultiplayerProject/Source/Headers/Engine/Reflection/Reflection.h
@@ -1,10 +1,21 @@
 #pragma once
 
+#include <assert.h>
+
 struct MetaType
 {
 	virtual double ToNumber(void* v) const = 0;
 	virtual const char* Name() const = 0;
 	virtual size_t SizeOf() const = 0;
+	virtual void Cast(void* to, void* from, const MetaType& fromType) const = 0;
+};
+
+struct VoidMetaType : public MetaType
+{
+	virtual double ToNumber(void* v) const override { return 0; }
+	virtual const char* Name() const override { return (char*)"void"; }
+	virtual size_t SizeOf() const override { return 0; }
+	virtual void Cast(void* to, void* from, const MetaType& fromType) const;
 };
 
 struct IntMetaType : public MetaType
@@ -12,6 +23,7 @@ struct IntMetaType : public MetaType
 	virtual double ToNumber(void* v) const override { return *(int*)v; }
 	virtual const char* Name() const override { return "int"; }
 	virtual size_t SizeOf() const override { return sizeof(int); }
+	virtual void Cast(void* to, void* from, const MetaType& fromType) const;
 };
 
 struct FloatMetaType : public MetaType
@@ -19,6 +31,7 @@ struct FloatMetaType : public MetaType
 	virtual double ToNumber(void* v) const  override { return *(float*)v; }
 	virtual const char* Name() const  override { return "float"; }
 	virtual size_t SizeOf() const  override { return sizeof(float); }
+	virtual void Cast(void* to, void* from, const MetaType& fromType) const;
 };
 
 struct DoubleMetaType : public MetaType
@@ -26,6 +39,7 @@ struct DoubleMetaType : public MetaType
 	virtual double ToNumber(void* v) const  override { return *(double*)v; }
 	virtual const char* Name() const override { return "double"; }
 	virtual size_t SizeOf() const override { return sizeof(double); }
+	virtual void Cast(void* to, void* from, const MetaType& fromType) const;
 };
 
 template <typename T>
@@ -34,11 +48,13 @@ class MetaTypeTemplate : public MetaType
 	virtual double ToNumber(void* v) const { return 0; };
 	virtual const char* Name() const { return (char*)"MetaTypeTemplate"; };
 	virtual size_t SizeOf() const { return sizeof(T); };
+	virtual void Cast(void* to, void* from, const MetaType& fromType) const {};
 };
 
-const MetaType& GetGlobalMetaType(int);
+const MetaType& GetGlobalMetaType(const int&);
 const MetaType& GetGlobalMetaType(const float&);
 const MetaType& GetGlobalMetaType(const double&);
+const MetaType& GetGlobalMetaType(void*);
 template<typename T>
 const MetaType & GetGlobalMetaType(const T &)
 {
@@ -87,7 +103,191 @@ private:\
 	const unsigned int offset;\
 	const unsigned int size;\
 	const MetaType& metaType;\
+};\
+\
+class FunctionSignature\
+{\
+public:\
+	template <typename Ret>\
+	FunctionSignature(Ret(Class::*)())\
+	{\
+		ret = &GetMetaTypeByType<Ret>();\
+		args = 0;\
+		argCount = 0;\
+	}\
+\
+	template <typename Ret, typename Arg0>\
+	FunctionSignature(Ret(Class::*)(Arg0))\
+	{\
+		ret = &GetMetaTypeByType<Ret>();\
+		static const MetaType* arguments[1] = { &GetMetaTypeByType<Arg0>() };\
+		args = arguments;\
+		argCount = 1;\
+	}\
+\
+	template <typename Ret, typename Arg0, typename Arg1>\
+	FunctionSignature(Ret(Class::*)(Arg0, Arg1))\
+	{\
+		ret = &GetMetaTypeByType<Ret>();\
+		static const MetaType* arguments[2] = { &GetMetaTypeByType<Arg0>(), &GetMetaTypeByType<Arg1>() };\
+		args = arguments;\
+		argCount = 2;\
+	}\
+\
+	template <typename Ret, typename Arg0, typename Arg1, typename Arg2>\
+	FunctionSignature(Ret(Class::*)(Arg0, Arg1, Arg2))\
+	{\
+		ret = &GetMetaTypeByType<Ret>();\
+		static const MetaType* arguments[3] = { &GetMetaTypeByType<Arg0>(), &GetMetaTypeByType<Arg1>(), &GetMetaTypeByType<Arg2>() };\
+		args = arguments;\
+		argCount = 3;\
+	}\
+\
+	template <typename Ret, typename Arg0, typename Arg1, typename Arg2, typename Arg3>\
+	FunctionSignature(Ret(Class::*)(Arg0, Arg1, Arg2, Arg3))\
+	{\
+		ret = &GetMetaTypeByType<Ret>();\
+		static const MetaType* arguments[4] = { &GetMetaTypeByType<Arg0>(), &GetMetaTypeByType<Arg1>(), &GetMetaTypeByType<Arg2>() , &GetMetaTypeByType<Arg3>() };\
+		args = arguments;\
+		argCount = 4;\
+	}\
+\
+	const MetaType* RetType() const { return ret; }\
+	const MetaType* ArgType(int idx) const { return args[idx]; }\
+	int ArgCount() const { return argCount; }\
+\
+private:\
+	const MetaType* ret;\
+	const MetaType** args;\
+	int argCount;\
+};\
+\
+struct MetaFunction : public AutoLister<MetaFunction>\
+{\
+public:\
+	template <typename Fun>\
+	MetaFunction(const char* name, Fun fun) :\
+		m_name(name),\
+		m_sig(fun)\
+	{\
+		m_fun = (void(Class::*)())fun;\
+		m_applyWrapper = &ApplyWrapper<Fun, Class>;\
+	}\
+\
+	const char* Name() const { return m_name; }\
+	const MetaType* RetType() const { return m_sig.RetType(); }\
+	const MetaType* ArgType(int idx) const { return m_sig.ArgType(idx); }\
+	int ArgCount() const { return m_sig.ArgCount(); };\
+\
+	void Apply(Class* entity, Variable ret, Variable* args, int argCount) const\
+	{\
+		m_applyWrapper(entity, m_fun, ret, args, argCount);\
+	}\
+\
+private:\
+	const char* m_name;\
+	FunctionSignature m_sig;\
+	void(Class::*m_fun)();\
+	void(*m_applyWrapper)(Class* entity, void(Class::*fun)(), Variable, Variable*, int);\
+};\
+
+template <typename Type>
+const MetaType& GetMetaTypeByType()
+{
+	Type* t = nullptr;
+	return GetGlobalMetaType(*t);
 };
+
+template <>
+const MetaType& GetMetaTypeByType<void>();
+
+struct Variable
+{
+	Variable() : v(0), m(&GetMetaTypeByType<void>()) {}
+
+	template<typename T>
+	Variable(const T& t) : v((void*)&t), m(&GetGlobalMetaType(t)) {}
+
+	void* v;
+	const MetaType* m;
+};
+
+template <typename Fun, class Class>
+inline void ApplyWrapper(Class* classInstance, void(Class::*fun)(), Variable ret, Variable* args, int argCount)
+{
+	Apply(classInstance, (Fun)fun, ret, args, argCount);
+}
+
+template <class Class, typename Ret>
+inline void Apply(Class* classInstance, Ret(Class::*fun) (), Variable ret, Variable* args, int argCount)
+{
+	assert(argCount == 0);
+	Ret r = (classInstance->*fun)();
+	ret.m->Cast(ret.v, &r, GetMetaTypeByType<void>());
+}
+template <class Class>
+inline void Apply(Class* classInstance, void(Class::*fun) (), Variable ret, Variable* args, int argCount)
+{
+	assert(argCount == 0);
+	(classInstance->*fun)();
+}
+
+template <class Class, typename Ret, typename Arg0>
+inline void Apply(Class* classInstance, Ret(Class::*fun) (Arg0), Variable ret, Variable* args, int argCount)
+{
+	assert(argCount == 1);
+	Ret r = (classInstance->*fun)(*(Arg0*)args[0].v);
+	ret.m->Cast(ret.v, &r, GetGlobalMetaType(r));
+}
+template <class Class, typename Arg0>
+inline void Apply(Class* classInstance, void(Class::*fun) (Arg0), Variable ret, Variable* args, int argCount)
+{
+	assert(argCount == 1);
+	(classInstance->*fun)(*(Arg0*)args[0].v);
+}
+
+template <class Class, typename Ret, typename Arg0, typename Arg1>
+inline void Apply(Class* classInstance, Ret(Class::*fun) (Arg0, Arg1), Variable ret, Variable* args, int argCount)
+{
+	assert(argCount == 2);
+	Ret r = (classInstance->*fun)(*(Arg0*)args[0].v, *(Arg1*)args[1].v);
+	ret.m->Cast(ret.v, &r, GetGlobalMetaType(r));
+}
+template <class Class, typename Arg0, typename Arg1>
+inline void Apply(Class* classInstance, void(Class::*fun) (Arg0, Arg1), Variable ret, Variable* args, int argCount)
+{
+	assert(argCount == 2);
+	(classInstance->*fun)(*(Arg0*)args[0].v, *(Arg1*)args[1].v);
+}
+
+template <class Class, typename Ret, typename Arg0, typename Arg1, typename Arg2>
+inline void Apply(Class* classInstance, Ret(Class::*fun)(Arg0, Arg1, Arg2), Variable ret, Variable * args, int argCount)
+{
+	assert(argCount == 3);
+	Ret r = (classInstance->*fun)(*(Arg0*)args[0].v, *(Arg1*)args[1].v, *(Arg2*)args[2].v);
+	ret.m->Cast(ret.v, &r, GetGlobalMetaType(r));
+}
+template <class Class, typename Arg0, typename Arg1, typename Arg2>
+inline void Apply(Class* classInstance, void(Class::*fun)(Arg0, Arg1, Arg2), Variable ret, Variable * args, int argCount)
+{
+	assert(argCount == 3);
+	(classInstance->*fun)(*(Arg0*)args[0].v, *(Arg1*)args[1].v, *(Arg2*)args[2].v);
+}
+
+template <class Class, typename Ret, typename Arg0, typename Arg1, typename Arg2, typename Arg3>
+inline void Apply(Class* classInstance, Ret(Class::*fun) (Arg0, Arg1, Arg2, Arg3), Variable ret, Variable* args, int argCount)
+{
+	assert(argCount == 4);
+	Ret r = (classInstance->*fun)(*(Arg0*)args[0].v, *(Arg1*)args[1].v, *(Arg2*)args[2].v, *(Arg3*)args[3].v);
+	ret.m->Cast(ret.v, &r, GetGlobalMetaType(r));
+}
+template <class Class, typename Arg0, typename Arg1, typename Arg2, typename Arg3>
+inline void Apply(Class* classInstance, void(Class::*fun) (Arg0, Arg1, Arg2, Arg3), Variable ret, Variable* args, int argCount)
+{
+	assert(argCount == 4);
+	(classInstance->*fun)(*(Arg0*)args[0].v, *(Arg1*)args[1].v, *(Arg2*)args[2].v, *(Arg3*)args[3].v);
+}
+
 
 #define Networked(AuthorityType, var)\
 RegisterNetworkedVariable(AuthorityType, (BaseObject::MetaVariable*)&mv_##var);
@@ -95,3 +295,6 @@ RegisterNetworkedVariable(AuthorityType, (BaseObject::MetaVariable*)&mv_##var);
 #define CreateVariableMetadata(class, var, ...)\
 static MetaVariable mv_##var = MetaVariable(&var, #var, offsetof(class, var), sizeof(var));\
 __VA_ARGS__
+
+#define CreateClassFunctionMetadata(Class, func)\
+static MetaFunction mf_##func = MetaFunction(#func, &Class##::func)
