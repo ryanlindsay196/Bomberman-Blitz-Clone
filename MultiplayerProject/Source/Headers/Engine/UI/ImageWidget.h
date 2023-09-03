8e159220ce49308eefa5c04f79d2f8ce16130564
rlindsa@masonlive.gmu.edu <ryanlindsay196@gmail.com>
2023-09-02 22:44:11 -0400

    Images now override TryHandleMousePress. This prevents them from eating input meant for other widgets.

--- MultiplayerProject/Source/Headers/Engine/UI/ImageWidget.h
+++ MultiplayerProject/Source/Headers/Engine/UI/ImageWidget.h
@@ -8,6 +8,8 @@ public:
 	CreateClassMetadata(Image)
 	virtual void Initialize(Renderer* renderer, rapidjson::GenericArray<false, rapidjson::Value::ValueType>::PlainType* widgetData) override;
 	virtual void Draw(Renderer* renderer, const SDL_Rect& parentRectBounds, bool isAnyParentDirty) override;
+	
+	virtual InputResponse TryHandleMousePress(mathfu::Vector<int, 2> mousePressPosition) override { return InputResponse::UnHandled; }
 
 	std::shared_ptr<Texture> GetTexture() { return texture; }
 private:
